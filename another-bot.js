f0024fec3a854a8682ccbc42c60439668799cd9e
steamedapples <applechanzhiwei@yahoo.com>
2018-10-02 17:50:37 +0800

    Update another-bot.js

--- another-bot.js
+++ another-bot.js
@@ -17,10 +17,12 @@ client.on('message', message => {
     
     //help
     if (message.content.startsWith(prefix + "help")) {
-    	message.channel.send('Here's a list of functions you can use to interact with me!
+        var helpMsg = 'Here's a list of functions you can use to interact with me!
                              `help`   Pulls up the help menu. (Duh.)
         `ping`   To test if I' + "\'" 'm still up.
-        `echo`   Echoes your message. ~~Please stand at least 17m away from the bot for this to work!~~ ');
+        `echo`   Echoes your message. ~~Please stand at least 17m away from the bot for this to work!~~ '
+        
+    	message.channel.send(helpMsg);
   	}
     
     //ping
