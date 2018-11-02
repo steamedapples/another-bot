e09db5eee8225db1615b7ce4f09673b8ef9287a3
steamedapples <applechanzhiwei@yahoo.com>
2018-11-02 20:26:38 +0800

    Update another-bot.js

--- another-bot.js
+++ another-bot.js
@@ -14,7 +14,7 @@ client.on('ready', () => {
                     \n **echo**   `Echoes your message. (Users are required to stand at least 17m away from the bot for this to work.)`\
                     \n **flip**   `Flips a coin.` \
                     \n **8ball + question **  `Ask a question, get an answer.` \
-		    \n **roll**   `Roll dice. Uses XdY format. (X and Y must be explicitly stated as of now.)`"
+		    \n **roll**   `Roll dice. Uses XdY format.`"
 
 client.on('message', message => {
     
@@ -62,30 +62,45 @@ client.on('message', message => {
         message.channel.send("The :8ball: says: **" + eightballAns + "**");
     }
     
-    //diceroll
+    //diceroll	
     if (message.content.startsWith(prefix + "roll")) {
         var str = message.content.split(prefix + "roll")[1];
         var res = str.split("d");
     
-        var arr = [];
-	    var i;
-	    for (i = 0; i < res.length; i++) {
-        arr.push(Number(res[i]));
-	    }
+	var announce = "you rolled a total of **" + Sum + "**. (" + results + ")";
+	var errorS = "Error: please use the nDx format!"
+	var errorTB = "Error: it's over 9000!"
+	    
+	for (i = 0; i < res.length; i++) {
+		if (isNan(res[i]) || res.length !== 2) {
+			message.channel.send(errorS);
+		} else if (res[i] > 9000) {
+			message.channel.send(errorTB); 
+		} else {
+			var arr = [];
+	    		var i;
+	    		for (i = 0; i < res.length; i++) {
+        		arr.push(Number(res[i]));
+	    		}
     
-        var results = [];
-        var j;
-        for (j = 0; j < arr[0]; j++) {
-        var a = Math.floor(Math.random()*arr[1]+1);
-        results.push(a);
-        };
+        		var results = [];
+        		var j;
+        		for (j = 0; j < arr[0]; j++) {
+        		var a = Math.floor(Math.random()*arr[1]+1);
+        		results.push(a);
+        		};
     
-        function getSum(total, num) {
-        return total + num;
-        }
+        		function getSum(total, num) {
+        		return total + num;
+			var Sum = results.reduce(getSum);
+			message.reply(announce);
+        		}
+		}
+	    
         
-        var Sum = results.reduce(getSum);
-        var announce = "You rolled a total of **" + Sum + "**. (" + results + ")";
+        
+        
+        
         
         message.channel.send(announce);
     }
