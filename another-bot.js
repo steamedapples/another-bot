eedeb26f81b27bc295134841fc925365c97eb297
steamedapples <applechanzhiwei@yahoo.com>
2019-12-01 18:13:32 +0800

    Update another-bot.js

--- another-bot.js
+++ another-bot.js
@@ -29,7 +29,23 @@ client.on('message', message => {
 
 	//help
 	if (message.content.startsWith(prefix + "help")) {
-		message.channel.send(helpMsg);
+		const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Regular field title', 'Some value here')
+	.addBlankField()
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+		message.channel.send(exampleEmbed);
 	}
 
 	//ping
