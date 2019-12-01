0d614f062a37a02d70c5dd2de651503b4cd94c39
steamedapples <applechanzhiwei@yahoo.com>
2019-12-01 19:04:46 +0800

    Update another-bot.js

--- another-bot.js
+++ another-bot.js
@@ -52,18 +52,7 @@ client.on('message', message => {
   /*
    * Takes a Date object, defaults to current date.
    */
-  .setTimestamp()
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 1024 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+  .setTimestamp();
  
   message.channel.send({embed});
 	}
